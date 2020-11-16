93ef53e183ae410dba015d9241249652d9624bb8
Lee Palacios <vitualizz@gmail.com>
2020-11-15 22:47:24 -0500

    create project test with bad test and install chai

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const utils = require('./lib/Utils.js')
 
 const JSperantito = (config) => {
   this._enforce(config, ['token'])
+  this.baseUrl = isProduction ? 'https://api.sperant.com/v2' : 'https://api.eterniasoft.com/v2'
   this.token = config.token
 }
 
 JSperantito.prototype = {
   _enforce: utils._enforce 
+}
+
+function isProduction () {
+  process.env.ENVIRONMENT === 'production'
 }
 
 
--- test/Projects_test.js
+++ test/Projects_test.js
@@ -1,14 +1,9 @@
 var JSperantito = require('../')
 var assert = require('assert')
 
-describe('Constructor', () => {
+describe('Projects', () => {
   it('should return error when config is not present', (done) => {
     assert.throws(() => new JSperantito() )
-    done()
-  })
-
-  it('should succed when config are provided', (done) => {
-    assert.ok(() => new JSperantito({token: 'token'}) )
     done()
   })
 })
