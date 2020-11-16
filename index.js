7aa50d16e1641398684f8f186ff6639544960360
Lee Palacios <vitualizz@gmail.com>
2020-11-16 03:42:43 -0500

    create makeRequest

--- index.js
+++ index.js
@@ -2,17 +2,11 @@ const utils = require('./lib/utils.js')
 
 function JSperantito (config) {
   this._enforce(config, ['token'])
-  this.baseUrl = isProduction() ? 'https://api.sperant.com/v2' : 'https://api.eterniasoft.com/v2'
   this.token = config.token
 }
 
 JSperantito.prototype = {
   _enforce: utils._enforce 
 }
-
-function isProduction () {
-  process.env.NODE_ENV === 'production'
-}
-
 
 module.exports = JSperantito
