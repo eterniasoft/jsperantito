fc7e8b0bef4531d022dd9a94fc68d2b31cde0f69
Lee Palacios <vitualizz@gmail.com>
2020-11-16 05:43:58 -0500

    getProjects

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const { _enforce } = require('./lib/utils.js')
 const _makeRequest = require('./lib/makeRequest.js')
 
+const {
+  _enforce,
+  _validateFields
+} = require('./lib/utils.js')
+
+const {
+  getProjects,
+  getProject
+} = require('./lib/projects.js')
+
+
+// Sperantito
 function JSperantito (config) {
   this._enforce(config, ['token'])
   this.token = config.token
@@ -8,7 +19,10 @@ function JSperantito (config) {
 
 JSperantito.prototype = {
   _enforce: _enforce,
-  _makeRequest: _makeRequest
+  _validateFields: _validateFields,
+  _makeRequest: _makeRequest,
+  getProjects: getProjects,
+  getProject: getProject
 }
 
 module.exports = JSperantito
--- test/Projects_test.js
+++ test/Projects_test.js
@@ -1,22 +1,30 @@
 const JSperantito = require('../')
 const { assert, expect } = require('chai')
-const {
-  TOKEN,
-  TOKEN_UNAUTHORIZED
-} = require('../lib/variables.js')
+const { TOKEN } = require('../lib/variables.js')
 
 describe('Projects', () => {
-  var sperant = new JSperantito({token: TOKEN})
+  describe('Token Bad', () => {
+    const sperant = new JSperantito({token: 'wrong token'})
+    
+    it('expect return error for token not authorized on run getProjects', (done) => {
+      sperant.getProjects().then(res => console.log('faaa', res)).catch(e => console.log(e))
+      expect(sperant.getProjects()).to.throw()
+      done()
+    })
 
-  it('expect return error for token not authorized on run getProjects', (done) => {
-    var sperant = new JSperantito({token: 'wrong token'})
-    expect(sperant.getProjects()).to.throw(TOKEN_UNAUTHORIZED)
-    expect(sperant.getProjects(1)).to.throw(TOKEN_UNAUTHORIZED)
-    done()
+    it('expect return error for token not authorized on run getProject', (done) => {
+      const f1 = ( () => sperant.getProject(1).then(res => console.log(res)) )
+      f1()
+      expect(f1()).to.throw()
+      done()
+    })
   })
 
-  it('expect return all projects', (done) => {
-    expect(sperant.getProjects()).to.be.a('array')
-    done()
+  describe('Requests', () => {
+    it('expect return all projects', (done) => {
+      var sperant = new JSperantito({token: TOKEN})
+      expect(sperant.getProjects()).to.be.a('array')
+      done()
+    })
   })
 })
