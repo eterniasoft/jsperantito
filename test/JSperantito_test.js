eb560016c71219835c14c0502dcd55461c5367ba
Lee Palacios <vitualizz@gmail.com>
2020-11-16 03:16:12 -0500

    separate variables to file and best test constructor

--- test/JSperantito_test.js
+++ test/JSperantito_test.js
@@ -1,15 +1,19 @@
 var JSperantito = require('../')
 var { assert, expect } = require('chai')
-var should = require('chai').should()
+const {
+  DEFAULT_ERROR_CODE,
+  PARAMETERS_UNDEFINED,
+  PARAMETERS_REQUIRED
+} = require('../lib/Variables.js')
 
 describe('Constructor', () => {
-  it('should return error when config is not present', (done) => {
-    (() => new JSperantito()).should.throw()
+  it('expect error when config is not present', (done) => {
+    expect(() => new JSperantito()).to.throw(PARAMETERS_UNDEFINED).with.property('code', DEFAULT_ERROR_CODE)
     done()
   })
 
-  it('should succed when config are provided', (done) => {
-    expect(new JSperantito({token: 'token'})).to.be.ok
+  it('not expect error when config are provided', (done) => {
+    expect(() => new JSperantito({token: 'token'})).to.not.throw()
     done()
   })
 })
--- test/Projects_test.js
+++ test/Projects_test.js
@@ -1,10 +1,18 @@
-var JSperantito = require('../')
-var { assert, expect } = require('chai')
-var should = require('chai').should()
+const JSperantito = require('../')
+const { assert, expect } = require('chai')
+const { TOKEN } = require('../lib/Variables.js')
 
 describe('Projects', () => {
-  it('should return error when config is not present', (done) => {
-    assert.throws(() => new JSperantito() )
+  var sperant = new JSperantito({token: TOKEN})
+
+  it('expect return error for token not authorized on run method getProjects', (done) => {
+    var sperant = new JSperantito({token: 'wrong token'})
+    expect(sperant.getProjects()).to.throw(TypeError)
+    done()
+  })
+
+  it('expect return all projects', (done) => {
+    expect(sperant.getProjects()).to.be.a('array')
     done()
   })
 })
