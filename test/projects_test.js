9598b703dfa3f1159b93f8d865e0c579821ffc7e
Lee Palacios <vitualizz@gmail.com>
2020-11-16 13:12:58 -0500

    change name test files and module projects

---
+++ test/projects_test.js
@@ -0,0 +1,34 @@
+const JSperantito = require('../')
+const { assert, expect } = require('chai')
+const { TOKEN } = require('../lib/variables.js')
+
+describe('Projects', () => {
+  describe('Token Unauthorized', () => {
+    const sperant = new JSperantito({token: 'wrong token'})
+
+    it('expect return error for token not authorized on run getProjects', async () => {
+      try {
+        await sperant.getProjects()
+      } catch (e) {
+        expect(e).to.equal('No Autorizado')
+      }
+    })
+
+    it('expect return error for token not authorized on run getProject', async () => {
+      try {
+        await sperant.getProject(1)
+      } catch (e) {
+        expect(e).to.equal('No Autorizado')
+      }
+    })
+  })
+
+  describe('Requests', () => {
+    var sperant = new JSperantito({token: TOKEN})
+    it('expect return all projects', async () => {
+      const { data, code } = await sperant.getProjects()
+      expect(code).to.equal(200)
+      expect(data).to.be.a('object')
+    })
+  })
+})
