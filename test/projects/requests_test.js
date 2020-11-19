87a550c84ee2047ca18459e28ddaf88ac6053e56
Lee Palacios <vitualizz@gmail.com>
2020-11-18 21:47:25 -0500

    project reestructure list and get full

--- test/projects/requests_test.js
+++ test/projects/requests_test.js
@@ -1,19 +1,23 @@
 require('module-alias/register')
 const JSperantito = require('@root')
 const { expect } = require('chai')
+const { TEST_TOKEN } = process.env
 
 const sperant = new JSperantito()
+sperant.configure({token: TEST_TOKEN})
 
 describe('Projects - Requests', () => {
-  it('Obtain list of projects', (done) => {
-    const VERSION = require('@root/package').version
-    expect(sperant.VERSION).to.equal(VERSION)
-    done()
+  it('should get data of project', async () => {
+    const { data, code } = await sperant.project(19).get()
+
+    expect(code).to.equal(200)
+    expect(data.project).to.be.a('object')
   })
 
-  it('Get a project', (done) => {
-    const VERSION = require('@root/package').version
-    expect(sperant.VERSION).to.equal(VERSION)
-    done()
+  it('should return projects list', async () => {
+    const { data, code } = await sperant.project().list()
+
+    expect(code).to.equal(200)
+    expect(data.projects).to.be.a('array')
   })
 })
