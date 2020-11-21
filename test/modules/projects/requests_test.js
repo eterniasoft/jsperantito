22da163a182ff50318b42110be3d67cae4533c99
Lee Palacios <vitualizz@gmail.com>
2020-11-20 22:09:53 -0500

    delete all modules

--- test/modules/projects/requests_test.js
+++ test/modules/projects/requests_test.js
@@ -5,23 +5,10 @@ const tData = require('./data.json')
 
 describe('Projects - Requests', () => {
   it('should get data of project', async () => {
-    const { data, code } = await sperant.project(tData.project.id).get()
+    const { data, code } = await sperant.project.getProject(tData.project.id)
+    console.log(code, data)
 
     expect(code).to.equal(200)
     expect(data.project).to.be.a('object')
-  })
-
-  it('should return projects list', async () => {
-    const { data, code } = await sperant.project().list()
-
-    expect(code).to.equal(200)
-    expect(data.projects).to.be.a('array')
-  })
-
-  it('should return projects filtering with q', async () => {
-    const { data, code } = await sperant.project().list({q: tData.projects.q})
-
-    expect(code).to.equal(200)
-    expect(data.projects).to.be.a('array')
   })
 })
