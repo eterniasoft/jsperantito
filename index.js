c1efe759dbd606073f9f945c3ee8502535761fc8
Lee Palacios <vitualizz@gmail.com>
2020-11-23 18:55:27 -0500

    routing for production

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-require('module-alias/register')
+const moduleAlias = require('module-alias')
+moduleAlias.addAliases({
+  '@root': __dirname,
+  '@lib': __dirname + '/lib',
+  '@test': __dirname + '/test',
+  '@controllers': __dirname + '/lib/controllers'
+})
+moduleAlias()
+
 const dotenv = require('dotenv')
 dotenv.config()
 
