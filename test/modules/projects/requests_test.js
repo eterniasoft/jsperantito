02b120d70da1689470bdc318e1360142c6eeb278
Lee Palacios <vitualizz@gmail.com>
2020-11-23 02:02:29 -0500

    only Api V3

--- test/modules/projects/requests_test.js
+++ test/modules/projects/requests_test.js
@@ -4,11 +4,19 @@ const sperant = require('@test/modules/init.js')
 const tData = require('./data.json')
 
 describe('Projects - Requests', () => {
-  it('should get data of project', async () => {
+  it('should get project - getProject', async () => {
     const { data, code } = await sperant.project.getProject(tData.project.id)
-    console.log(code, data)
 
     expect(code).to.equal(200)
     expect(data.project).to.be.a('object')
   })
+
+  /* Only API V3
+  it('should get types by project - getTypesByProject', async () => {
+   const { data, code } = await sperant.project.getTypesByProject(tData.project.id)
+
+    expect(code).to.equal(200)
+    expect(data.project).to.be.a('object')
+  })
+  */
 })
