3639b5e5f277af2f8ee309ea3cdcf53a48774cae
Lee Palacios <vitualizz@gmail.com>
2020-12-30 09:59:45 -0500

    dotenv and up version

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const JSperantito = require('./lib/jsperantito.js')
 
 function createInstance () {
--- test/modules/init.js
+++ test/modules/init.js
@@ -1,5 +1,4 @@
 const sperant = require('../../index.js')
-const { TEST_TOKEN } = process.env
-sperant.configure({token: '1b7e8fa834d376146f5024ab8767e1e54da6bdf01df76248d6bace11db5eb442'})
+sperant.configure({token: process.env.TEST_TOKEN })
 
 module.exports = sperant
